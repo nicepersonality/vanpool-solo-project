1b077e7f946a518c998304a8ad4267daca6d9d7c
Lilith Bentley <sbentley@gmail.com>
2019-10-24 16:28:15 -0500

    Remove unused db setup route

--- server/routes/day.router.js
+++ server/routes/day.router.js
@@ -121,20 +121,4 @@ router.put('/drive/:dateId', rejectUnauthenticated, (req, res) => {
     });
 })
 
-// Handles POST request with new date data
-router.post('/add', rejectUnauthenticated, (req, res, next) => {
-  const id = req.body.id;
-  const date = req.body.date;
-  const year = req.body.year;
-  const week = req.body.week;
-  const month = req.body.month;
-  const weekday = req.body.weekday;
-  const day = req.body.day;
-  const queryText = 'INSERT INTO "days" (id, date, year, week, month, weekday, day) VALUES ($1, $2, $3, $4, $5, $6, $7)';
-  const queryValues = [id, date, year, week, month, weekday, day];
-  pool.query(queryText, queryValues)
-    .then(() => res.sendStatus(201))
-    .catch(() => res.sendStatus(500));
-});
-
 module.exports = router;
