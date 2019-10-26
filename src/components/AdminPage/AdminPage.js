a9a4f71e8feac7bad4c5b72a70a6e41533950481
Lilith Bentley <sbentley@gmail.com>
2019-10-26 17:34:17 -0500

    Stub out Admin page and require authorization

---
+++ src/components/AdminPage/AdminPage.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+class AdminPage extends Component {
+  componentDidMount() {
+    this.checkEditAccess();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.match !== this.props.match) {
+      this.checkEditAccess();
+    }
+    if (prevProps.store.user !== this.props.store.user) {
+      this.checkEditAccess();
+    }
+  }
+
+  checkEditAccess() {
+    if (this.props.location.hash === '#edit') {
+      if ((this.props.store.access_level > 2)) {
+        this.setState({ editMode: true });
+      } else {
+        this.props.history.push({ pathname: this.props.location.pathname, hash: '' });
+      }
+    } else {
+      this.setState({ editMode: false });
+    }
+  }
+
+  userAccessDescribe(access) {
+    access = parseInt(access);
+    if (access === 0) return 'Unconfirmed user';
+    if (access === 1) return 'Inactive user';
+    if (access === 2) return 'Vanpool member';
+    if (access > 2) return 'Administrator';
+    return '';
+  }
+
+  render() {
+    return (
+      <div className="AdminPage-component">
+        {/* Make sure the user is authorized */}
+        {(this.props.store.user.access_level > 2)
+          ? <div className="adminTools">
+            <p>Admin controls go here</p>
+          </div>
+          : <div className="adminTools -unauthorized">
+            <h3>Unauthorized</h3>
+            <p>Sorry! You must be an administrator to view this page.</p>
+          </div>
+        }
+        <hr /><pre className="wrapper -thin">this.props=
+        {JSON.stringify(this.props, null, 2)}</pre>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = store => ({
+  store,
+});
+export default withRouter(connect(mapStateToProps)(AdminPage));
+
