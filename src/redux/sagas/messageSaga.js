d6911e2eba4ffb7394c2530092bbb3ea2df6ff74
Lilith Bentley <sbentley@gmail.com>
2019-10-28 08:51:50 -0500

    Add Saga to send and receive messages from the API

---
+++ src/redux/sagas/messageSaga.js
@@ -0,0 +1,39 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+// worker Saga: will be fired on "FETCH_MESSAGES" actions
+function* fetchMessages(action) {
+  try {
+    const config = {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    };
+
+    const response = yield axios.get(`/api/messages/${action.payload.date}`, config);
+
+    yield put({ type: 'SET_MESSAGES', payload: response.data });
+  } catch (error) {
+    console.log('Message get request failed', error);
+  }
+}
+
+// worker Saga: will be fired on "ADD_MESSAGE" actions
+function* addMessage(action) {
+  try {
+    const config = {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    };
+    yield axios.post('/api/messages', action.payload, config);
+    yield fetchMessages();
+  } catch (error) {
+    console.log('Route update request failed', error);
+  }
+}
+
+function* messageSaga() {
+  yield takeLatest('FETCH_MESSAGES', fetchMessages);
+  yield takeLatest('ADD_MESSAGE', addMessage);
+}
+
+export default messageSaga;
